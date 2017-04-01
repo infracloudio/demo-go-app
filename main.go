d043156a883b3c6e2f452ec8ab52617b7a0dd8d8
Harshal Shah <harshal@infracloud.io>
2017-04-01 19:00:14 +0000

    Making brown canary

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func newInstance() *Instance {
 	i.Version = version
 	return i
 }
-const version string = "1.1.0"
+const version string = "1.1.2"
  
 func main(){
 http.HandleFunc("/", indexHandler)
