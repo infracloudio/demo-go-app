9bafbe5671b859584d43092b557d648d529ad77d
Vishal Biyani <vishal@infracloud.io>
2018-05-16 15:55:14 +0530

    Added more handlers, a build complete dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-FROM golang:1.6.3-onbuild
+ARG GO_VERSION=1.9.2
+FROM golang:${GO_VERSION}
+
+ENV GOPATH /usr
+ENV APP	   ${GOPATH}/src/github.com/infracloudio/demo-go-app/
+
+ADD main.go ${APP}
+ADD html.go ${APP}
+
+WORKDIR ${APP}
+RUN go get
+RUN go build -o /goapp main.go html.go
+
+ENTRYPOINT ["/goapp"]
+EXPOSE 8000
--- main.go
+++ main.go
@@ -1,33 +1,42 @@
 package main
- 
-import(
-"fmt"
-"net/http"
-"os"
-"time"
-"html/template"
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Instance struct {
-        Pod        string
-        TS         string
-        Version    string
+	Pod     string
+	TS      string
+	Version string
 }
 
- 
-func indexHandler( w http.ResponseWriter, r *http.Request){
-tpl := template.Must(template.New("out").Parse(html))
-i := &Instance{}
-i = newInstance()
-tpl.Execute(w, i)
- 
-}
-
-func healthHandler( w http.ResponseWriter, r *http.Request){
-fmt.Fprintf(w, "I'm fine Version : %s ", version)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL)
+	tpl := template.Must(template.New("out").Parse(html))
+	i := &Instance{}
+	i = newInstance()
+	tpl.Execute(w, i)
 
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL)
+	fmt.Fprintf(w, "I'm fine Version : %s ", version)
+}
+
+func testoneHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL)
+	fmt.Fprintf(w, "I'm Test ONE 11111111111")
+}
+
+func testtwoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL)
+	fmt.Fprintf(w, "I'm Test TWO 2222222222222")
+}
 
 func newInstance() *Instance {
 	var i = new(Instance)
@@ -36,10 +45,14 @@ func newInstance() *Instance {
 	i.Version = version
 	return i
 }
+
 const version string = "1.1.1"
- 
-func main(){
-http.HandleFunc("/", indexHandler)
-http.HandleFunc("/healthz", healthHandler)
-http.ListenAndServe(":8000",nil)
+
+func main() {
+	fmt.Println("Starting the goapp at port 8000")
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/healthz", healthHandler)
+	http.HandleFunc("/testone", testoneHandler)
+	http.HandleFunc("/testtwo", testtwoHandler)
+	http.ListenAndServe(":8000", nil)
 }
